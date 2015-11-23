43e99d9d3f5f438dad4692a04e5a6d5515821463
Samuel Bronson <naesten@gmail.com>
2015-11-23 17:31:30 -0500

    Add a new skipped test based on OmniSharp
    This test demonstrates a consequence of the lack of documentation
    for MefHostServices, and it could also demonstrate a problem with its
    implementation -- it's kind of hard to say without documentation.

---
+++ src/Workspaces/CoreTest/MefHostServicesTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.CodeAnalysis.Host.Mef;
+using Xunit;
+
+namespace Microsoft.CodeAnalysis.UnitTests
+{
+    public class MefHostServicesTests
+    {
+        /// <summary>
+        /// Based on https://github.com/OmniSharp/omnisharp-roslyn/blob/v1.6.3/src/OmniSharp.Roslyn/HostServicesBuilder.cs
+        /// in the case where no <see cref="ICodeActionProvider"/>s
+        /// are passed in.
+        /// </summary>
+        // FIXME: Document what is required to avoid System.Reflection.ReflectionTypeLoadException here
+        [Fact(Skip = "Is this not supposed to be done? The documentation for MefHostServices leaves something to be desired.")]
+        public void SimpleCreateTest()
+        {
+            var mhs = MefHostServices.Create(MefHostServices.DefaultAssemblies);
+            // If this hasn't thrown, we've passed.
+        }
+    }
+}
